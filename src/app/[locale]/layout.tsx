c4c6d80525e37ff3ed3646ab6bc1f9ecda772619
claudiobenegiamo <claudio.benegiamo@gmail.com>
2025-02-20 12:33:24 +0100

    add google meta

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@
 
 import {ReactNode} from "react";
 import {SessionProvider} from "next-auth/react";
+import Head from "next/head";
 
 type MainLayoutProps = Readonly<{
   children: ReactNode;
@@ -10,6 +11,9 @@ type MainLayoutProps = Readonly<{
 export default function MainLayout({children}: MainLayoutProps) {
   return (
     <SessionProvider>
+      <Head>
+        <meta name="google-site-verification" content="Z8NLIvW2401bzDzN1e3lPLul7sRbAEgUmpDDkvHd1O8"/>
+      </Head>
       {children}
     </SessionProvider>
   );
